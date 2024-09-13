e26e7f8e46fa408dd9157fc406e615d0d3958a9b
jl54 <jonas@jonasluethi.dev>
2024-09-14 00:44:21 +0200

    Playing around with templates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"book-library/db"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,24 @@ import (
 type SimpleResponse struct {
 	Status int    `json:"status"`
 	Data   string `json:"data"`
+}
+
+func renderIndex(w http.ResponseWriter, r *http.Request) {
+	var t, err = template.ParseFiles("templates/layout.html", "templates/body.html")
+
+	if err != nil {
+		log.Fatalf("Unable to parse files %v\n", err)
+	}
+
+	err = t.Execute(w, nil)
+
+	if err != nil {
+		log.Fatalf("Unable to parse template %v\n", err)
+	}
+}
+
+func handlePublic() http.Handler {
+	return http.StripPrefix("/public/", http.FileServer(http.Dir("public")))
 }
 
 func main() {
@@ -26,6 +45,8 @@ func main() {
 
 	registerRoutes(dbpool)
 
+	http.Handle("/public/", handlePublic())
+	http.HandleFunc("/", renderIndex)
 	// ListenAndServe always returns an error,
 	// since it only returns when an unexpected error occurs.
 	// In order to log that error we wrap the
